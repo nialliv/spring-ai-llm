02960d20cb1818b975ce1ed0e013683d3af448bc
nialliv <artemjevnikita@ya.ru>
2025-08-04 22:43:37 +0400

    tried integration

--- src/main/java/ru/artemev/springaillm/SpringAiLlmApplication.java
+++ src/main/java/ru/artemev/springaillm/SpringAiLlmApplication.java
@@ -1,13 +1,25 @@
 package ru.artemev.springaillm;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringAiLlmApplication {
 
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder builder) {
+        return builder.build();
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringAiLlmApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(SpringAiLlmApplication.class, args);
+        ChatClient chatClient = context.getBean(ChatClient.class);
+
+        System.out.println(chatClient.prompt().user("Дай первую строчку из Войны и мир").call().content());
+
     }
 
 }
