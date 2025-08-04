7a9a228b7d6cd613a329378f38b9a434f9ea90ca
nialliv <artemjevnikita@yandex.ru>
2025-08-04 14:47:44 +0400

    init

---
+++ src/main/java/ru/artemev/springaillm/SpringAiLlmApplication.java
@@ -0,0 +1,13 @@
+package ru.artemev.springaillm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiLlmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiLlmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/artemev/springaillm/SpringAiLlmApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.artemev.springaillm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiLlmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
